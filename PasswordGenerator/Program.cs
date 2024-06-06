90f42bfc679430cd8ae53bd9352553c1228799fb
ENDISit <mr.ilia08@mail.ru>
2024-06-07 00:22:25 +0300

    Добавьте файлы проекта.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+class PasswordGenerator
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите длину пароля:");
+        int length = int.Parse(Console.ReadLine());
+
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+        Random random = new Random();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < length; i++)
+        {
+            password.Append(chars[random.Next(chars.Length)]);
+        }
+
+        Console.WriteLine($"Сгенерированный пароль: {password}");
+
+        Console.WriteLine("Нажмите любую клавишу для выхода...");
+        Console.ReadLine();
+    }
+}
